f5bdd340556e5ba3384b15f2b6b16e37dd615295
talkme803-ai <talkme803@gmail.com>
2026-05-16 01:30:34 +0500

    Update Firebase configuration with new credentials

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YAHAN_APIKEY",
-  authDomain: "YAHAN_AUTHDOMAIN",
-  projectId: "YAHAN_PROJECTID",
-  storageBucket: "YAHAN_BUCKET",
-  messagingSenderId: "YAHAN_MSGID",
-  appId: "YAHAN_APPID"
+  apiKey: "AIzaSyBoGFcskXPam7DJCacJ71wlK8bMCbpbyuM",
+  authDomain: "cargo-tracking-9c0aa.firebaseapp.com",
+  projectId: "cargo-tracking-9c0aa",
+  storageBucket: "cargo-tracking-9c0aa.firebasestorage.app",
+  messagingSenderId: "862830098744",
+  appId: "1:862830098744:web:8a6e222ea1e497e4bda9cd"
 };
 
 const app = initializeApp(firebaseConfig);
